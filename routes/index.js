469f7ee7baa12d978d2d8a8a2091d5ba43e302c7
yon11b <qorgkdus12@naver.com>
2023-07-10 21:40:22 +0900

    [Add] cors

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,15 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const cors = require('cors');
+
+const options={
+    origin: "http://localhost:3000",
+    credentials: true,
+    optionSuccessStatus: 200
+};
 
 //router.use('/rest', require('./rest/'));
+router.use(cors(options));
 router.use('/rest/search', require('./rest/recruits'));
 
 module.exports = router;
