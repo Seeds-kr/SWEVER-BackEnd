9010f1b20ad526191e7381967b3e75d1907d15e8
Hyeongjin <lipfolnp@gmail.com>
2023-07-17 21:44:23 +0900

    [Fix]띄어쓰기 수정

--- models/index.js
+++ models/index.js
@@ -3,7 +3,7 @@ const path = require('path');
 const Sequelize = require('sequelize');
 
 const basename = path.basename(module.filename);
-const env = process.env.NODE_ENV ||'development';
+const env = process.env.NODE_ENV || 'development';
 const config = require(path.join(__dirname,'..','config','config.json'))[env].db;
 const db = {};
 
