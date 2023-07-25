7108c9b4389dd1ce348fd0eb0b1bf5a6c8ad94b5
Hyeongjin <lipfolnp@gmail.com>
2023-07-25 20:54:09 +0900

    [Fix]main응답 수정

--- routes/rest/main/main.js
+++ routes/rest/main/main.js
@@ -5,11 +5,15 @@ const tech_stack = require('../../../models/tech_stack');
 
 async function getMain(req, res) {
     try {
-        const resp = await models.recruit_post.findAll({
+        const remote = await models.recruit_post.findAll({
             attributes: 
                 ['company_name', 'company_logo','description_title',
                  'is_visa_sponsored','is_remoted','location', 'posted_date',
             ],
+            where: {
+                is_remoted: 1,
+                is_visa_sponsored: 0
+            },
             limit: 6,
             order: [
                 ['posted_date', 'DESC']
@@ -29,13 +33,41 @@ async function getMain(req, res) {
                 }
             ]
         });
-
+        const visa = await models.recruit_post.findAll({
+            attributes: 
+                ['company_name', 'company_logo','description_title',
+                 'is_visa_sponsored','is_remoted','location', 'posted_date',
+            ],
+            where: {
+                is_remoted: 0,
+                is_visa_sponsored: 1
+            },
+            limit: 6,
+            order: [
+                ['posted_date', 'DESC']
+            ],
+            include: [
+                {
+                    model: models.nation,
+                    attributes: []
+                    
+                },                
+                {
+                    model: models.tech_stack,
+                    through: {
+                        attributes: []  // 중간 테이블에서 가져올 속성 지정
+                    },
+                    attributes: [] // tech_stack에서 가져올 속성 지정                    
+                }
+            ]
+        });
         
         res.send([{
             Message: "Success", 
             ResultCode: "ERR_OK",            
-            Size: 6,
-            Response: resp
+            Size: 12,
+            Remote: remote,
+            Visa: visa
         }])
     }
     catch (err) {
