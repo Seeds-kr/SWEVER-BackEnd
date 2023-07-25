986dd08d9a67c32d03fd0232d276216be76dd938
Hyeongjin <lipfolnp@gmail.com>
2023-07-25 21:02:58 +0900

    [Fix]충돌 수정

--- routes/rest/index.js
+++ routes/rest/index.js
@@ -5,12 +5,6 @@ const router = express.Router();
 router.use('/main', require('./main'));
 router.use('/search', require('./recruits'));
 router.use('/detail', require('./detail'));
-<<<<<<< HEAD
 router.use('/auth', require('./auth'));
-=======
-<<<<<<< HEAD
-=======
 
->>>>>>> 8b333db6219dd2247b7cef5db39c66f914b0c08f
->>>>>>> 3f92cf9df364f0484f60fba312d82dae28372916
 module.exports = router;
