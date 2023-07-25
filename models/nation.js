4e219162d172437de9b57bfadccf212b0d93e8d0
yon11b <qorgkdus12@naver.com>
2023-07-25 22:55:36 +0900

    [Fix] pk id error

--- models/nation.js
+++ models/nation.js
@@ -22,11 +22,7 @@ module.exports = (sequelize, DataTypes)=>{
 
     // Foreign keys
     nation.associate = (models)=> {
-<<<<<<< HEAD
-        nation.hasMany(models.recruit_post, {foreignKey: 'nation_id',sourceKey:'nation_id'});
-=======
         nation.hasMany(models.recruit_post, { foreignKey: 'nation_id', targetKey: 'id' });
->>>>>>> a9e0e30c126015cae0623c40bdbb5de4eb59672d
     }
 
     return nation;
