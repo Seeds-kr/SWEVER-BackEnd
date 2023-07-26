0bd41032817729cda494648e1f1a5aa0e34db4d9
Hyeongjin <lipfolnp@gmail.com>
2023-07-26 22:04:43 +0900

    [Add]주석 추가

--- passport/index.js
+++ passport/index.js
@@ -4,10 +4,10 @@ const kakao = require('./kakaoStrategy');
 const models = require('../models')
 
 module.exports = () => {
-    passport.serializeUser((user, done) => {
+    passport.serializeUser((user, done) => {  // user = exUser
         done(null, user.id) // user id 만 추출
     });
-
+    // 세션 { 123: 1 } - { 세션쿠키: 유저아이디 } -> 메모리에 저장 
     passport.deserializeUser((id, done) => {
         models.user.findOne({ where: { id }})
             .then((user) => done(null, user))
