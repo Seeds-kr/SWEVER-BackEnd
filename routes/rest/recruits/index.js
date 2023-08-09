3b54c8d09d2494d93fc27c2f8f9cd95f574abe55
yon11b <qorgkdus12@naver.com>
2023-08-09 16:15:17 +0900

    [Add] init_nation_list

--- routes/rest/recruits/index.js
+++ routes/rest/recruits/index.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
-const { getRecruits_pagination } = require('./recruit.js');
+const { getRecruits_pagination, getInit } = require('./recruit.js');
 
+router.get('/init', getInit);
 router.get('/:page', getRecruits_pagination);
 
 router.post('/', (req, res)=>{
     res.status(405).send({
             Message: "Method not allowed", 
-            ResultCode: "ERR_INVALID_DATA"            
+            ResultCode: "ERR_INVALID_DATA"
         });
     return res;
 });
@@ -16,7 +17,7 @@ router.post('/', (req, res)=>{
 router.use((req, res, next) => {
     res.status(400).send({
         Message: "Invalid parameter", 
-        ResultCode: "ERR_INVALID_PARAMETER"   
+        ResultCode: "ERR_INVALID_PARAMETER"
     });
     return res;
 });
--- routes/rest/recruits/recruit.js
+++ routes/rest/recruits/recruit.js
@@ -5,9 +5,11 @@ const { Op } = require('sequelize');
 
 async function getRecruits_pagination(req, res) {
     const pageNum = req.params.page;
+    
     const query_keyword = req.query.keyword;    
     const query_remote = req.query.remote;
     const query_visa = req.query.visa;
+
     const whereCondition = {};
     const limit = 10;
     const offset = limit * (parseInt(pageNum) - 1);
@@ -25,8 +27,6 @@ async function getRecruits_pagination(req, res) {
     }
 
     try {
-        const nation = await models.nation_continent.findAll();            
-
         let post = await models.recruit_post.findAll({
             attributes: 
                 ['id','nation_id','company_name','description_title','description_content',
@@ -50,7 +50,8 @@ async function getRecruits_pagination(req, res) {
                     model: models.description_tech,
                     attributes: ['tech_name']
                 },
-            ]
+            ],
+            limit: 100 // 나중에 redis 써서 캐시에 저장하면 필요없어질 코드. 임시방편으로 일단 100개만 가져오도록함.
         });
 
         post = post.filter((app, idx) => (offset <= idx && idx <= offset + limit - 1));
@@ -59,7 +60,6 @@ async function getRecruits_pagination(req, res) {
             ResultCode: "ERR_OK",            
             Size: 10,
             Response: {
-                nation_continent_list: nation,
                 recruit_post_list: post
             }
         })
@@ -73,6 +73,27 @@ async function getRecruits_pagination(req, res) {
     }
 }
 
-module.exports = {    
-    getRecruits_pagination
+async function getInit(req, res) {
+    try {
+        const nation = await models.nation_continent.findAll();            
+        res.send({
+            Message: "Success", 
+            ResultCode: "ERR_OK",
+            Response: {
+                nation_continent_list: nation
+            }
+        })
+    }
+    catch (err) {
+        console.log(err);        
+        res.status(500).send({            
+            Message: "Internal server error", 
+            ResultCode: "ERR_INTERNAL_SERVER"
+        });
+    }
+}
+
+module.exports = {
+    getRecruits_pagination,
+    getInit
 };
