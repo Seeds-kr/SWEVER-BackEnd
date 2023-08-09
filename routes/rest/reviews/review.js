83556babceba3cf102c178caac19017ab2e64681
yon11b <qorgkdus12@naver.com>
2023-08-09 16:16:13 +0900

    [Add] review-return user(5)&crawling(5)

--- routes/rest/reviews/review.js
+++ routes/rest/reviews/review.js
@@ -5,23 +5,40 @@ const app = require("../../../app");
 async function getReviews_pagination(req, res) {
   const pageNum = req.params.page;
 
-  const limit = 10;
+  const limit = 5;
   const offset = limit * (parseInt(pageNum) - 1);
   try {
-    let resp = await models.review.findAll({
-      group: ["id"],
-      attributes: ["id", "title", "link", "thumbnail", "created_at"],
+    let resp1 = await models.review.findAll({      
+      attributes: ["id", "title", "content", "thumbnail", "created_at"],
       order: [["id", "DESC"]],
+      where:{
+        'google_api':'0'
+      }
     });
-    resp = resp.filter(
+    resp1 = resp1.filter(
       (app, idx) => offset <= idx && idx <= offset + limit - 1
     );
+
+    let resp2 = await models.review.findAll({      
+      attributes: ["id", "title", "link", "thumbnail", "created_at"],
+      order: [["id", "DESC"]],
+      where:{
+        'google_api':'1'
+      }
+    });
+    resp2 = resp2.filter(
+      (app, idx) => offset <= idx && idx <= offset + limit - 1
+    );
+
     res.send([
       {
         Message: "Success",
         ResultCode: "ERR_OK",
         Size: 10,
-        Response: resp,
+        Response: {
+          'user_data':resp1,
+          'crawling_data':resp2
+        }          
       },
     ]);
   } catch (err) {
@@ -42,7 +59,6 @@ async function upsertReview(req, res) {
       title: req.body.title,         
       creator_id: req.user.id,
       thumbnail: req.body.thumbnail,
-      snippet: req.body.snippet,
       updated_at: Date.now(),
       content: req.body.content,
       google_api:"0"
