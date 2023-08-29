3494d91f325600d78c2324f444153884ec79fc3e
yon11b <qorgkdus12@naver.com>
2023-08-29 16:16:03 +0900

    [Add] review- single access

--- routes/rest/reviews/index.js
+++ routes/rest/reviews/index.js
@@ -2,20 +2,21 @@ const express = require('express');
 const router = express.Router();
 const { isLoggedIn } = require('../../../middlewares');
 
-
 const {
     getReviews_pagination,    
+    getReview,
     deleteReview,
     upsertReview
 } = require('./review.js');
 
-router.get('/:page', getReviews_pagination);
+router.get('/', getReviews_pagination);
+router.get('/:id', getReview);
 router.delete('/:id', isLoggedIn, deleteReview);
 router.post('/', isLoggedIn, upsertReview);
 
 router.use((req, res, next) => {
     res.status(400).send([{
-        Message: "Invalid parameter", 
+        Message: "Invalid parameter",
         ResultCode: "ERR_INVALID_PARAMETER"   
     }]);
     return res;
